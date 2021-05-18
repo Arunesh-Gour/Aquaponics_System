4837b4dec489d277c5bf0db03091fd0575cb068f
Arunesh-Gour <arunesh.pratap2018@vitbhopal.ac.in>
2021-05-18 13:15:13 +0530

    EmulatedHardware - WaterHeater implementation complete.

--- src/AquaponicsSystem/EmulatedHardware/Controllers/WaterHeater.py
+++ src/AquaponicsSystem/EmulatedHardware/Controllers/WaterHeater.py
@@ -5,6 +5,7 @@ if ("WaterHeater" not in (IoTDevice.IoTDevice.deviceTypes[0]).keys()):
       {
          # "attribute_name" : default_value_in_apt_format,
          "power" : False,
+         "_heatingRate" : 0.0,
       },
       0, # IoTDevice manages this automatically.
    ]
@@ -13,11 +14,16 @@ class WaterHeater (IoTDevice.IoTDevice):
    def __init__ (self, deviceName=None):
       super().__init__(deviceType="WaterHeater", deviceName=deviceName)
       
-      Signal.Signal.register(self.serial, self.read(key="?"))
+      Signal.Signal.register(self.serial, self.write(key="_?"))
+   
+   def delete (self):
+      Signal.Signal.unregister(self.serial)
+      
+      super().delete()
    
    def read (self, key):
       if (type(key).__name__ == 'str'):
-         if (key.strip().lower() == '' or len(key.strip().lower()) < 1):
+         if (key.strip() == '' or len(key.strip()) < 1):
             raise ValueError("unrecognized key '{0}'".format(\
                   key,
                )\
@@ -25,30 +31,34 @@ class WaterHeater (IoTDevice.IoTDevice):
       else:
          raise TypeError("key requires 'str' only")
       
-      # Returns a tuple of what values are readable.
-      if (key.strip().lower() == "?"):
-         return ("serial", "power",)
+      key = key.strip()
       
-      if (key.strip().lower() == "serial"):
+      # Returns a tuple of what values are readable.
+      if (key == "?"):
+         return ("serial", "power", "_heatingRate")
+      
+      if (key == "serial"):
          return self.serial
       
       # Basic check.
-      if (key not in self.deviceAttributes.keys()):
+      if (key not in self.write(key="_?")):
          raise NameError("unrecognized key '{0}'".format(key))
       
       # Main read method start here:
-      if (key.strip().lower() == "power"):
+      if (key == "power"):
          # Explicitly checking so as not to pass mutable value.
          if (self.deviceAttributes["power"] == True):
             return True
          else:
             return False
+      if (key == "_heatingRate"):
+         return self.deviceAttributes["_heatingRate"]
       else:
          raise ValueError("non-readable key '{0}'".format(key))
    
    def write (self, key, value=None):
       if (type(key).__name__ == 'str'):
-         if (key.strip().lower() == '' or len(key.strip().lower()) < 1):
+         if (key.strip() == '' or len(key.strip()) < 1):
             raise ValueError("unrecognized key '{0}'".format(\
                   key,
                )\
@@ -56,31 +66,44 @@ class WaterHeater (IoTDevice.IoTDevice):
       else:
          raise TypeError("key requires 'str' only")
       
+      key = key.strip()
+      
       # Returns a tuple of what values are mutable.
-      if (key.strip().lower() == "?"):
+      if (key == "?"):
          # Contains visible values.
          return ("power",)
-      elif (key.strip().lower() == "_?"):
+      elif (key == "_?"):
          # Contains hidden values as well.
-         return ("power",)
+         return ("power", "_heatingRate",)
       
       # Basic check.
-      if (key not in self.deviceAttributes.keys()):
+      if (key not in self.write(key="_?")):
          raise NameError("unrecognized key '{0}'".format(key))
       
       # Main write method start here:
-      if (key.strip().lower() == "power"):
+      if (key == "power"):
          if (type(value).__name__ == 'bool'):
             oldValue = self.deviceAttributes["power"]
-            if (value == True):
-               self.deviceAttributes["power"] = True
-            elif (value == False):
-               self.deviceAttributes["power"] = False
+            
+            self.deviceAttributes["power"] = value
             
             if (oldValue != self.deviceAttributes["power"]):
                Signal.Signal.signal(self.serial, "power")
          else:
             raise TypeError("power requires 'bool' only")
+      if (key == "_heatingRate"):
+         if (type(value).__name__ == 'float'):
+            if (value > -5.0 and value < 5.0):
+               oldValue = self.deviceAttributes["_heatingRate"]
+               
+               self.deviceAttributes["_heatingRate"] = value
+               
+               if (oldValue != self.deviceAttributes["_heatingRate"]):
+                  Signal.Signal.signal(self.serial, "_heatingRate")
+            else:
+               raise ValueError("invalid _heatingRate '{0}'".format(value))
+         else:
+            raise TypeError("_heatingRate requires 'float' value")
       else:
          raise ValueError("immutable key '{0}'".format(key))
 # Guard line below: Do NOT exceed 79 character limit per line in any case.
